da00d88f04ff30566beaa77a37db2997bb64975a
Eitan Adler <lists@eitanadler.com>
2013-04-11 22:24:30 -0400

    correctly fix warnings

--- routing.c
+++ routing.c
@@ -7,7 +7,7 @@ lock the table before calling
 */
 void print_routing_table(){
 	for(size_t i = 0; i < MAX_HOSTS; ++i){
-		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
+		printf("To: %zd; next hop: %zd, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
 		for (size_t k = 0; k < MAX_HOSTS; ++k) {
 			printf("%ld ", routing_table[i].pathentries[k] ? k : -1);
 	
@@ -21,7 +21,7 @@ lock the table before calling
 */
 void print_rt_ptr(struct route* table){
 	for(size_t i = 0; i < MAX_HOSTS; ++i){
-		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, table[i].next_hop, table[i].distance);
+		printf("To: %ld; next hop: %zd, dist: %zd path contains: ", i, table[i].next_hop, table[i].distance);
 		for (size_t k = 0; k < MAX_HOSTS; ++k) {
 			printf("%ld ", table[i].pathentries[k] ? k : -1);
 	
