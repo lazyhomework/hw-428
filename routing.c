9c8dbe583852f56ba092fdfe41206c06d1476a4a
Eitan Adler <lists@eitanadler.com>
2013-04-11 21:51:49 -0400

    Fix routing print

--- routing.c
+++ routing.c
@@ -19,7 +19,7 @@ lock the table before calling
 */
 void print_rt_ptr(struct route* table){
 	for(size_t i = 0; i < MAX_HOSTS; ++i){
-		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
+		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, table[i].next_hop, table[i].distance);
 		for (size_t k = 0; k < MAX_HOSTS; ++k) {
 			printf("%ld ", table[i].pathentries[k] ? k : -1);
 	
