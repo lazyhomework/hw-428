d6f095faaeca947bfa90e59b7c0516d8c1ffa918
Eitan Adler <lists@eitanadler.com>
2013-04-11 21:26:57 -0400

    fix warnings

--- routing.c
+++ routing.c
@@ -5,9 +5,9 @@ lock the table before calling
 */
 void print_routing_table(){
 	for(size_t i = 0; i < MAX_HOSTS; ++i){
-		printf("To: %d; next hop: %d, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
+		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
 		for (size_t k = 0; k < MAX_HOSTS; ++k) {
-			printf("%d ", routing_table[i].pathentries[k] ? k : -1);
+			printf("%ld ", routing_table[i].pathentries[k] ? k : -1);
 	
 		}
 		printf("\b\n");
@@ -19,9 +19,9 @@ lock the table before calling
 */
 void print_rt_ptr(struct route* table){
 	for(size_t i = 0; i < MAX_HOSTS; ++i){
-		printf("To: %d; next hop: %d, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
+		printf("To: %ld; next hop: %ld, dist: %u path contains: ", i, routing_table[i].next_hop, routing_table[i].distance);
 		for (size_t k = 0; k < MAX_HOSTS; ++k) {
-			printf("%d ", table[i].pathentries[k] ? k : -1);
+			printf("%ld ", table[i].pathentries[k] ? k : -1);
 	
 		}
 		printf("\b\n");
