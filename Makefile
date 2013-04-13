c2de03366aa169711d50330c3c4694142e74cdc9
KevinMilcz <kmilcze1@binghamton.edu>
2013-04-12 20:46:34 -0400

    Changed debug code in server to use print_memblock Added Makefile changes for degbug.h & c

--- Makefile
+++ Makefile
@@ -2,14 +2,16 @@ CC=clang
 CFLAGS=	-Wall -Wextra -g -pedantic -pthread -Wno-padded
 LDFLAGS= -pthread
 
-server:	server.o config.o packets.o routing.o packets.h config.h routing.h
-	$(CC) $(CFLAGS) $(LDFLAGS) server.o config.o packets.o routing.o -o server
+server:	server.o config.o packets.o routing.o debug.o packets.h config.h routing.h debug.h
+	$(CC) $(CFLAGS) $(LDFLAGS) server.o config.o packets.o routing.o debug.o -o server
 
 config.o: config.h
 
 packets.o: packets.h config.h
 
 routing.o: routing.h config.h
+
+debug.o: debug.h
 
 clean:
 	rm -vf *.o server
--- debug.c
+++ debug.c
@@ -1,5 +1,11 @@
 #include "debug.h"
 
+/*
+Print a block of memory
+starting from buf, prints hex values in 4 byte groups with
+a new line evey rowwidth bytes.
+For a single line, use rowwidth = size or 0
+*/
 void print_memblock(void *buf, size_t size, size_t rowwidth){
 
 	unsigned char * print_buf = (unsigned char *)buf;
@@ -9,7 +15,8 @@ void print_memblock(void *buf, size_t size, size_t rowwidth){
 	for(size_t i = 0; i < size; ++i){
 		if(i%4 == 0){
 			printf(" ");
-		}else if(i%rowwidth == 0){
+		}
+		if(i%rowwidth == 0){
 			printf("\n");
 		}
 		printf("%02x",print_buf[i]);
